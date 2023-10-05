fe374d5b5cd1ce1716082d62b7d3e0f00cb5d9d3
ok <111350417+Gazioss@users.noreply.github.com>
2023-10-05 13:04:27 +0000

    d

--- list.c
+++ list.c
@@ -22,20 +22,20 @@ void add(node *head, int x) {
 // exersice 3.b
 int size(node *l) { 
 
-assert(l != NULL); // check 
+assert(l != NULL); 
 
 int counter = 0; 
 
 node *p = l; 
 
 
-while(p->next != NULL){
-  p = p->next;
-  counter++;
+while(p->next != NULL){ // loop that runs as long as the current node, is not the last. 
+  p = p->next; // set the current node to the next node 
+  counter++; // increment counter by one for each repetition of the loop 
 }
 
   return counter; 
-	return 0;
+
 }
 
 // exersice 3.c and 3.d
@@ -44,9 +44,9 @@ void printout(node *l) {
   //       The last element's next is NULL
   // post: The values of the list are printed out
   node *p = l->next;
-  while (p != NULL) {
+  while (p != NULL) { 
     printf("%d, ", p->data);
-    p = p->next; // go to the next node in the linked list.
+    p = p->next; // go to the next node in the linked list. 
   }
   printf("\n");
 }
@@ -59,22 +59,22 @@ int largest(node *l) {
 
 
 assert(l != NULL);
-int largestValue = 0;
-node *p = l; 
+int largestValue = 0; // initialize largest value as 0 
+node *p = l->next; 
 
 
-while(p->next != NULL){
+while(p->next != NULL){ // loop that runs as long as the current node is not the last node. 
   
-  if(p->data > largestValue){
-    largestValue = p->data;
+  if(p->data > largestValue){ // check if the data stored by the current node, is larger than the largest value 
+    largestValue = p->data; // if yes, change the largest value to the data stored by the current node. 
   }
 
 
-  p = p->next;
+  p = p->next; // next node 
   
 }
 
-  return largestValue;
+  return largestValue; 
 
 }
 
